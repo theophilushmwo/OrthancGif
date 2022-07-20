e5e2cc13ce7fa31f5ec27bbf94b904b0b192e301
Théophilus Homawoo <83962803+theophilushmwo@users.noreply.github.com>
2022-07-20 20:37:04 +0200

    Edited Class

--- MIPGenerator.py
+++ MIPGenerator.py
@@ -1,55 +1,40 @@
-import matplotlib.pyplot as plt
 import numpy as np 
 import scipy.ndimage
-from PIL import Image
 import os 
-from fpdf import FPDF
 import imageio.v2 as imageio
+
 import requests
 import io
 
 class MIPGenerator:
     def __init__(self, numpy_array:np.ndarray):
         self.numpy_array = numpy_array
+        if len(self.numpy_array.shape) != 3:
+            raise Exception('numpy_array must be 3D')
 
-    def project(self, angle:int) -> np.ndarray:
-        if len(self.numpy_array.shape) == 4 : 
-            array = np.amax(self.numpy_array, axis = -1)
-        else : 
-            array = self.numpy_array
+    def _project(self, angle:int) -> np.ndarray:
+        array = self.numpy_array
         axis = 1
-        vol_angle = scipy.ndimage.interpolation.rotate(array , angle=angle , reshape=False, axes = (1,2))
+        vol_angle = scipy.ndimage.interpolation.rotate(array, angle=angle, reshape=False, axes = (1,2))
         MIP = np.amax(vol_angle, axis=axis)
-        self.MIP = MIP
         return MIP
 
-    def create_projection_list(self, frames:int=60) -> list:
+    def _create_projection_list(self, frames:int=60) -> list:
         angles = np.linspace(0, 360, frames)
         projection_list = []
         for i in angles:
-            self.project(i)
-            self.flip_MIP()
-            projection_list.append(self.MIP)
-            print(i)
+            MIP = self._project(i)
+            MIP = self.flip_MIP(MIP)
+            projection_list.append(MIP)
+            print(f'{i} degrees')
         return projection_list
 
     def create_gif(self, filename:str, directory:str, frames:int=60, delay:float=10):
         delay = delay/1000
         filepath = os.path.join(directory, filename + '.gif')
-        projection_list = self.create_projection_list(frames)
+        projection_list = self._create_projection_list(frames)
         imageio.mimsave(filepath, projection_list, duration=delay)
 
-    def flip_MIP(self):
-        self.MIP = np.flip(self.MIP, axis=0)
-        return self.MIP
-
-        
-x = requests.get('https://demo.orthanc-server.com/series/318603c5-03e8cffc-a82b6ee1-3ccd3c1e-18d7e3bb/numpy?=rescale=true')
-
-c = np.load(io.BytesIO(x.content), allow_pickle=True)
-
-test = MIPGenerator(c)
-
-test.create_gif('test', '/Users/theophilushomawoo/Documents/stage/galeo/python/', 100, 50)
-
-
+    def flip_MIP(self, MIP:np.ndarray):
+        MIP = np.flip(MIP, axis=0)
+        return MIP
