ab73e8c46c17d71a8dcabd96b75426925edcbcd7
Théophilus Homawoo <83962803+theophilushmwo@users.noreply.github.com>
2022-09-26 13:24:48 +0200

    Multithreading

--- python/mip.py
+++ python/mip.py
@@ -1,6 +1,11 @@
 import numpy as np
 import scipy.ndimage
 import imageio
+from multiprocessing import Pool, cpu_count
+from functools import partial
+import io
+import requests
+import time
 
 class MIPGenerator:
     def __init__(self, numpy_array: np.ndarray, frames, delay, projection):
@@ -15,21 +20,21 @@ class MIPGenerator:
         vol_angle = scipy.ndimage.rotate(
             self.numpy_array, angle=angle, reshape=False, axes=(1, 2))
         MIP = np.amax(vol_angle, axis=1)
+        MIP = np.flip(MIP, axis=0)
         return MIP
 
     def _create_projection_list(self) -> list:
         angles = np.linspace(0, self.projection, self.frames)
-        projection_list = []
-        for i in angles:
-            MIP = self._project(i)
-            MIP = self.flip_MIP(MIP)
-            projection_list.append(MIP)
+        nbCores = cpu_count() - 2
+        # Create a pool with the function project
+        pool = Pool(nbCores)
+        projection_list = pool.map(self._project, angles)
         return projection_list
 
     def create_gif(self, output) -> None:
+        print('Creating gif...')
+        timer = time.time()
         projection_list = self._create_projection_list()
+        print('Projection time: ', time.time() - timer)
         imageio.mimwrite(output, projection_list, format='.gif', duration=self.delay)
 
-    def flip_MIP(self, MIP: np.ndarray):
-        MIP = np.flip(MIP, axis=0)
-        return MIP
